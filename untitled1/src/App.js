54bfc517a86f9aa3d607bb75a78f4f2eb038169c
Antonio <63789531+dumitrutony03@users.noreply.github.com>
2024-03-28 20:58:55 +0200

    To DO: De configurat REACT-ul oentru noile fisiere (Login, Register, Dashboard). - erori la compilare - executare npm start

--- untitled1/src/App.js
+++ untitled1/src/App.js
@@ -1,30 +1,21 @@
 import React from 'react';
-import RegisterForm from './RegisterForm';
-import LoginForm from './LoginForm';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignUpPage';
+import Dashboard from "./components/Dashboard";
 
 function App() {
     return (
         <div className="App">
-            <h1>Register New Investor</h1>
-            <RegisterForm />
-        </div>
-    );
+            <Router>
 
-    const onRegister = (name, email, password) => {
-        // Similar to the login, implement the registration logic here.
-        console.log('Register with:', name, email, password);
-    };
+                <Routes>
+                    <Route path="/login" element={<LoginPage/>} />
+                    <Route path="/register" element={ <SignupPage/>} />
+                    <Route path = "/dashboard" element={<Dashboard/>}/>
+                </Routes>
 
-    const onLogin = (email, password) => {
-        // Implement the login logic here. For example, use fetch or axios to send a POST request to your Spring Boot backend.
-        console.log('Login with:', email, password);
-    };
-
-    return (
-        <div>
-            <RegisterForm onRegister={onRegister} />
-            <LoginForm onLogin={onLogin} />
+            </Router>
         </div>
     );
 }
